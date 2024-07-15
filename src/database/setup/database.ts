8fc8782a32e6674898abf4f8f4270e6228052bf0
gabriel <gabrielmasquediez@gmail.com>
2024-07-15 22:46:30 +0200

    prettier

--- src/database/setup/database.ts
+++ src/database/setup/database.ts
@@ -1,14 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const todoSequelize = new Sequelize(
+dotenv.config();
+
+const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
-  process.env.DB_PASS as string,
+  process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT as string, 10),
     dialect: "mysql",
-    port: parseInt(process.env.DB_PORT || "3306"),
   }
 );
 
-export default todoSequelize;
+export default sequelize;
